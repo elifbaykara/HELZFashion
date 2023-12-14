5009ef3b9c16a68dc009de8a39dd651109364002
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-14 20:53:01 +0300

    [Feat] controller updated
    category controller updated, clothes controller added

---
+++ Presentation/HELZFashion.MVC/Controllers/ClothesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HELZFashion.MVC.Controllers
+{
+    public class ClothesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
