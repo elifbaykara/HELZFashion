5989a0ecb3e63a914b57fb684289299ab63ac9dd
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-12-14 23:12:48 +0300

    [Fix] Order Update

--- Core/HELZFashion.Domain/Enums/OrderStatus.cs
+++ Core/HELZFashion.Domain/Enums/OrderStatus.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HELZFashion.Domain.Enums
 {
-    internal class OrderStatus
+    public enum OrderStatus
     {
+        Pending = 1,
+        Shipped = 2,
+        Delivered = 3,
+        Canceled = 4
     }
 }
