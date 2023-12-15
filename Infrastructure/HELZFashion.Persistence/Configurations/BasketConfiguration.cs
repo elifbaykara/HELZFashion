b8013ca277d3f5aff3c50713c9a0049c78d69be1
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-12-15 22:19:20 +0300

    conf

---
+++ Infrastructure/HELZFashion.Persistence/Configurations/BasketConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HELZFashion.Persistence.Configurations
+{
+    public class BasketConfiguration
+    {
+    }
+}
