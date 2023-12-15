333e2039513680ceabde4aa421e7991db717bed9
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-12-15 21:01:33 +0300

    [Feat] Create Login and Sign Up Page

--- Core/HELZFashion.Domain/Entities/Order.cs
+++ Core/HELZFashion.Domain/Entities/Order.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HELZFashion.Domain.Entities
 {
-    internal class Order
+    public class Order
     {
     }
 }
--- Infrastructure/HELZFashion.Persistence/Contexts/HELZFashionDbContext.cs
+++ Infrastructure/HELZFashion.Persistence/Contexts/HELZFashionDbContext.cs
@@ -12,7 +12,12 @@ namespace HELZFashion.Persistence.Context
 {
     public class HELZFashionDbContext : DbContext
     {
+       
+       public DbSet<Basket> Baskets { get; set; }
+       public DbSet<Brand> Brands { get; set; }
        public DbSet<Category> Categories { get; set; }
+       public DbSet<Clothes> Clothes { get; set; }
+       public DbSet<Order> Orders { get; set; }
         public HELZFashionDbContext(DbContextOptions<HELZFashionDbContext> dbContextOptions) : base(dbContextOptions)
         {
 
