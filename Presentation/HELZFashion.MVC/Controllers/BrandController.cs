2b62d5a1e0476f301043f4b19b7eb1d8da2c3f65
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-12-15 22:26:48 +0300

    add brand controller

---
+++ Presentation/HELZFashion.MVC/Controllers/BrandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HELZFashion.MVC.Controllers
+{
+    public class BrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
