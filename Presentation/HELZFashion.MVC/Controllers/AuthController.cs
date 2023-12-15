4cc6f5ad3e758c9c1d3f81d87f8075d1355f0d43
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-12-15 22:01:41 +0300

    [Feat] Login and Sign Up Page

---
+++ Presentation/HELZFashion.MVC/Controllers/AuthController.cs
@@ -0,0 +1,107 @@
+﻿using HELZFashion.Domain.Identity;
+using HELZFashion.MVC.ModelViews;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+
+namespace HELZFashion.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IToastNotification _toastNotification;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(UserManager<User> userManager, IToastNotification toastNotification, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _toastNotification = toastNotification;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var registerViewModel = new AuthRegisterViewModel();
+
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(AuthRegisterViewModel registerViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(registerViewModel);
+            }
+
+            var userId = Guid.NewGuid();
+
+            var user = new User()
+            {
+                Id = userId,
+                Email = registerViewModel.Email,
+                UserName = registerViewModel.Username,
+                FirstName = registerViewModel.FirstName,
+                LastName = registerViewModel.LastName,
+                BirthDate = registerViewModel.BirthDate.ToUniversalTime(),
+                Gender = registerViewModel.Gender,
+                CreatedOn = DateTimeOffset.UtcNow,
+                CreatedByUserId = userId.ToString(),
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+            if(!identityResult.Succeeded)
+            {
+                foreach(var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+
+            _toastNotification.AddSuccessToastMessage("Kayıt oluşturuldu");
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var loginViewModel = new AuthLoginViewModel();
+
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(AuthLoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+
+            if(user is null)
+            {
+                _toastNotification.AddErrorToastMessage("Email ya da şifre hatalı");
+
+                return View(loginViewModel);
+            }
+
+            var loginResult = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password,true,true);
+
+            if(!loginResult.Succeeded)
+            {
+                _toastNotification.AddErrorToastMessage("Email ya da şifre hatalı");
+
+                return View(loginViewModel);
+            }
+
+            _toastNotification.AddSuccessToastMessage($"Başarıyla giriş yapıldı.Hoş geldin {user.UserName}");
+            
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Presentation/HELZFashion.MVC/ModelViews/AuthLoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HELZFashion.MVC.ModelViews
+{
+    public class AuthLoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
