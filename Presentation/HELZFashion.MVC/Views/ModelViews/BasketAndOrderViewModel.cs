0db601d9b32a2bf59a82d53e2953219f1d527e82
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-12-17 18:57:33 +0300

    [Feat] Create Controller Views

---
+++ Presentation/HELZFashion.MVC/Views/ModelViews/BasketAndOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HELZFashion.Domain.Entities;
+using HELZFashion.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HELZFashion.MVC.ViewModels
+{
+    public class BasketAndOrderViewModel
+    {
+        public Basket Basket { get; set; }
+
+        [Required(ErrorMessage = "Shipping Address is required.")]
+        public string ShippingAddress { get; set; }
+
+        [Required(ErrorMessage = "Payment Method is required.")]
+        public PaymentMethod Payment { get; set; }
+    }
+}
