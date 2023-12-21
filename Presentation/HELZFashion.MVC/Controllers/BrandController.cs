a054dc468af24b541a69c6c61422f256c15857c4
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-21 15:43:09 +0300

    [Feat] UpdatedUI
    Controllers and views updated

--- Presentation/HELZFashion.MVC/Controllers/BrandController.cs
+++ Presentation/HELZFashion.MVC/Controllers/BrandController.cs
@@ -1,7 +1,9 @@
 ﻿using HELZFashion.Domain.Entities;
+using HELZFashion.MVC.Models;
 using HELZFashion.Persistence.Context;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HELZFashion.MVC.Controllers;
 
@@ -49,14 +51,48 @@ public class BrandController : Controller
     }
 
     [HttpGet]
-    public IActionResult DeleteBrand(string id)
-    {
+    public IActionResult Delete(string id)
+    { 
+
         var brand = _context.Brands.Where(x => x.Id == Guid.Parse(id)).FirstOrDefault();
 
         _context.Brands.Remove(brand);
 
+        brand.DeletedByUserId = "LivanurErdem";
+        brand.DeletedOn = DateTime.UtcNow;
+        brand.IsDeleted = true;
+
         _context.SaveChanges();
 
-        return RedirectToAction("index");
+        return RedirectToAction("Index");
     }
+    [HttpGet]
+    public IActionResult Update(Guid id)
+    {
+
+        var brands = _context.Brands.ToList();
+
+        var upClothes = new ClothesAddBrandCategory
+        {
+          Brands = brands 
+        };
+
+        return View(upClothes);
+    }
+    [HttpPost]
+    public IActionResult Update(Guid id, string Name, string Description)
+    {
+        var brand = _context.Brands.FirstOrDefault(x => x.Id == id);
+
+        if (brand != null)
+        {
+            brand.Name = Name;
+            brand.Description = Description;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return RedirectToAction("Index");
+    }
+
 }
